f96bb36e613003f1a9a734e31dbbbde299d0f240
Angelo Ricardo <angeloengcomp@gmail.com>
2023-08-06 01:35:24 -0300

    feat: implements get user token by gmail

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,37 +1,44 @@
 import {
   View,
   Text,
-  Image,
-  TextInput,
   TouchableWithoutFeedback,
   Keyboard,
   KeyboardAvoidingView,
   Platform,
   TouchableOpacity,
-  Alert,
   ActivityIndicator,
   ImageBackground,
 } from 'react-native';
 
-import React, { useState } from 'react';
-import { FontAwesome } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
+import React, { useEffect, useState } from 'react';
 import { colors } from '@theme/colors';
-import { formStyles, typography } from '@theme/globalStyles';
-import Logo from '../../../assets/icon.png';
-import { useLogin } from '@hooks/useLogin';
 import backgroundImg from '../../assets/background.png';
+import * as Google from 'expo-auth-session/providers/google';
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env';
 export function LoginScreen() {
-  const [email, onChangeEmail] = useState('');
-  const [password, onChangePassword] = useState('');
-  const { mutate, isLoading } = useLogin();
+  const [loading, setLoading] = useState(false);
 
-  function handleLogin() {
-    if (!email || !password) {
-      return Alert.alert('Preencha todos os campos');
-    }
-    mutate({ email, password });
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  });
+
+  function handleSignIn() {
+    setLoading(true);
+    googleSignIn().then((res) => {
+      if (res.type !== 'success') {
+        setLoading(false);
+      }
+    });
   }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      console.log(response.authentication?.idToken);
+      setLoading(false);
+    }
+  }, [response]);
 
   return (
     <KeyboardAvoidingView
@@ -44,45 +51,14 @@ export function LoginScreen() {
             style={{ flex: 1, justifyContent: 'center', marginVertical: 32 }}
           >
             <View style={{ paddingHorizontal: 40 }}>
-              <View>
-              </View>
+              <View></View>
               <View style={{ marginBottom: 32 }}>
-                <Text className='text-primary text-center text-2xl font-bold'>Ignite Fleet</Text>
-                <Text className='text-secondary text-center'>
+                <Text className="text-primary text-center text-2xl font-bold">
+                  Ignite Fleet
+                </Text>
+                <Text className="text-secondary text-center">
                   Gestão de uso de veiculos
                 </Text>
-              </View>
-              <View style={formStyles.compactInputWrapper}>
-                <FontAwesome
-                  name="user"
-                  size={24}
-                  color={colors.primaryBlack}
-                />
-                <TextInput
-                  style={formStyles.compactInput}
-                  onChangeText={onChangeEmail}
-                  keyboardType="email-address"
-                  placeholder="E-mail"
-                  value={email}
-                />
-              </View>
-
-              <View style={{ marginTop: 8, marginBottom: 32 }}>
-                <View style={formStyles.compactInputWrapper}>
-                  <FontAwesome
-                    name="lock"
-                    size={24}
-                    color={colors.primaryBlack}
-                  />
-                  <TextInput
-                    style={formStyles.compactInput}
-                    onChangeText={onChangePassword}
-                    secureTextEntry={true}
-                    maxLength={6}
-                    value={password}
-                    placeholder="Senha"
-                  />
-                </View>
               </View>
             </View>
           </View>
@@ -90,10 +66,10 @@ export function LoginScreen() {
         <View style={{ paddingHorizontal: 40, marginBottom: 40 }}>
           <TouchableOpacity
             className="flex items-center justify-center h-12 bg-white rounded-lg"
-            onPress={handleLogin}
+            onPress={handleSignIn}
           >
             <Text style={{ color: colors.primary, fontWeight: '700' }}>
-              {isLoading ? <ActivityIndicator color={'#ffffff'} /> : 'Entrar'}
+              {loading ? <ActivityIndicator color={'#ffffff'} /> : 'Entrar'}
             </Text>
           </TouchableOpacity>
         </View>
